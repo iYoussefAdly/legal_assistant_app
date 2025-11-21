aee35a4b20e5214f7f1d521087e16c862dfcc5de
abdalla <swesyabdalla@gmail.com>
2025-11-21 15:40:41 +0200

    Chat Screen UI

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const styleBold24 = TextStyle(
+    fontFamily: "Inter",
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Color(0xff544F4F)
+  );
+  static const styleRegular20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: Color(0xff544F4F)
+  );
+  static const styleRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static const styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const styleRegular18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+  static const styleSemitBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: Colors.white
+  );
+  static const styleSemitBold14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+  static const styleMedium14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+}
