163b20fe473d5a4af0f91cc3d9184fba082d4cbe
youssefadly <youssefadly332@gmail.com>
2025-11-21 12:51:49 +0200

    add inter font

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class AppStyles {
+  static const styleBold24 = TextStyle(
+    fontFamily: "Inter",
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Color(0xff544F4F)
+  );
+  
+}
