eeb53f0cd92f5e62b26819c007e8f146a5badb43
abdalla <swesyabdalla@gmail.com>
2025-11-21 15:29:11 +0200

    Chat Screen Ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:legal_assistant_app/views/chat_view.dart';
 import 'package:legal_assistant_app/views/test_view.dart';
 
 void main() {
   runApp(const LegalAssistantApp());
 }
+
 class LegalAssistantApp extends StatelessWidget {
   const LegalAssistantApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const TestView(),
+      debugShowCheckedModeBanner: false,
+      home: const ChatView(),
     );
   }
 }
---
+++ lib/views/widgets/chat_view_body.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:legal_assistant_app/core/utils/app_styles.dart';
+import 'package:legal_assistant_app/views/widgets/custom_widget/bubble_chat.dart';
+import 'package:legal_assistant_app/views/widgets/custom_widget/bubble_chat_ai.dart';
+import 'package:legal_assistant_app/views/widgets/custom_widget/container_chat.dart';
+import 'package:legal_assistant_app/views/widgets/custom_widget/container_chat_bottom.dart';
+
+class ChatViewBody extends StatelessWidget {
+  const ChatViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final media = MediaQuery.of(context).size;
+    return Padding(
+      padding: EdgeInsets.only(
+        top: media.height * .1,
+        bottom: media.height * .02,
+        left: media.width * .03,
+        right: media.width * .03,
+      ),
+      child: Column(
+        spacing: media.height * .03,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ContainerChat(icon: CupertinoIcons.back),
+              Container(
+                width: media.width * .3,
+                height: media.height * .05,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(231, 214, 248, 1),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('New Chat', style: AppStyles.styleSemitBold14),
+                    SizedBox(width: media.width * .02),
+                    const Icon(Icons.keyboard_arrow_down_outlined, size: 30),
+                  ],
+                ),
+              ),
+              ContainerChat(icon: Icons.menu),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Divider(
+                  thickness: 2,
+                  color: Color.fromRGBO(153, 44, 230, 0.3),
+                ),
+              ),
+              Text('   Dec 15th   ', style: AppStyles.styleSemitBold16),
+              Expanded(
+                child: Divider(
+                  thickness: 2,
+                  color: Color.fromRGBO(153, 44, 230, 0.3),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: media.height * .52,
+            child: SingleChildScrollView(
+              child: Column(
+                spacing: media.height * .05,
+                children: [
+                  ChatBubbleAi(
+                    message:
+                        'Hello! I’m your Smart Legal Assistant.How can I help you today?',
+                    avatarImage: Image.asset('assets/images/bubble.png'),
+                  ),
+                  ChatBubble(
+                    message: ' Can my employer fire me without notice?',
+                  ),
+                  ChatBubbleAi(
+                    message:
+                        'In most cases, an employer must provide a notice period unless there is a serious violation stated in the contract.',
+                    avatarImage: Image.asset('assets/images/bubble.png'),
+                  ),
+                  ChatBubble(message: 'Can you give me reference?'),
+                  ChatBubbleAi(
+                    message: 'Reference:\nLabor Law – Article 75',
+                    avatarImage: Image.asset('assets/images/bubble.png'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ContainerChatBottom(
+            height: media.height * .15,
+            width: media.width,
+            borderRadius: 10,
+            child: Column(
+              spacing: media.height * .02,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Message Qanunay Ai....',
+                    hintStyle: AppStyles.styleRegular16,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: media.height * .02,
+                      horizontal: media.width * .04,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      child: ContainerChatBottom(
+                        height: media.height * .04,
+                        width: media.width * .12,
+                        borderRadius: 30,
+                        child: Icon(Icons.add, size: 25),
+                      ),
+                    ),
+                    GestureDetector(
+                      child: ContainerChatBottom(
+                        height: media.height * .04,
+                        width: media.width * .4,
+                        borderRadius: 30,
+                        child: Row(
+                          children: [
+                            Icon(Icons.travel_explore, size: 22),
+                            SizedBox(width: 6),
+                            Text(
+                              'Type your question',
+                              style: AppStyles.styleRegular14,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    GestureDetector(
+                      child: ContainerChatBottom(
+                        height: media.height * .04,
+                        width: media.width * .12,
+                        borderRadius: 30,
+                        child: Icon(Icons.mic, size: 25),
+                      ),
+                    ),
+                    GestureDetector(
+                      child: ContainerChatBottom(
+                        height: media.height * .05,
+                        width: media.width * .1,
+                        borderRadius: 30,
+                        child: Icon(Icons.arrow_outward_sharp, size: 25),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
